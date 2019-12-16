a9719e1821505d8d808d7fd80dda753f7aac8528
Hackour <hackour2@hotmail.com>
2019-12-17 01:00:26 +0200

    js-step7

---
+++ Exercises/Javascript-Basics-01/Step7.js
@@ -0,0 +1,11 @@
+var ss = prompt("enter your shoe size:");
+
+var by = prompt("enter your birth year: ");
+
+function step7() {
+  var s8 = ss * 2 + 5 * 50 - by + 1766;
+
+  return s8;
+}
+
+alert(step7());
