117f90606a55805729d55a2093784346e8d1df29
HasanAwad <hasan-awad@hotmail.com>
2019-12-18 17:03:32 +0200

    bs4

--- Exercises/Javascript-Basics-04/questions.js
+++ Exercises/Javascript-Basics-04/questions.js
@@ -1,101 +1,166 @@
 
 const stringSize = (text) => {
 
+    return text.length;
+
 }
 const replaceCharacterE = (text) => {
+
+    return text.replace(/e/,' ');
 
 }
 const concatString = (text1, text2) => {
 
+    return text1+text2;
+
 }
 const showChar5 = (text) => {
 
+    return text.charAt(4);
 }
 const showChar9 = (text) => {
+
+    return text.substring(0,9);
 
 }
 const toCapitals = (text) => {
 
+    return text.toUpperCase();
 }
 const toLowerCase = (text) => {
 
+    return text.toLowerCase();
+
 }
 const removeSpaces = (text) => {
+    return text.trim();
 
 }
 const IsString = (text) => {
+
+    return typeof text == "string";
 
 }
 
 const getExtension = (text) => {
 
+    let x = text.indexOf('.');
+    return text.substring(x+1,text.length);
+
 }
 const countSpaces = (text) => {
 
+    let count = 0;
+
+    for( let i =0;i<text.length;i++){
+        if (text.charAt(i)==" "){
+            count++;
+        }
+
+
+    }
+    
+ 
+    return count;
+
 }
 const InverseString = (text) => {
+
+    return text.split("").reverse().join("");
 
 }
 
 const power = (x, y) => {
 
+    return Math.pow(x,y);
+
 }
 const absoluteValue = (num) => {
+
+    return Math.abs(num);
 
 }
 const absoluteValueArray = (array) => {
 
-}
+   
+    return array.map(element => {
+
+        if (element < 0) {
+        
+          element = element * -1;
+        }
+        return element;
+      });
+    
+};
+
 const circleSurface = (radius) => {
+    return Math.round(Math.PI * Math.pow(radius, 2));
 
 }
 const hypothenuse = (ab, ac) => {
+    return Math.sqrt(Math.pow(ab, 2) + Math.pow(ac, 2));
 
 }
 const BMI = (weight, height) => {
+    return parseFloat((weight / Math.pow(height, 2)).toFixed(2));
 
 }
 
 const createLanguagesArray = () => {
 
+    return ["Html", "CSS", "Java", "PHP"];
+    
 }
 
 const createNumbersArray = () => {
 
+    return [0, 1, 2, 3, 4, 5];
 }
 
 const replaceElement = (languages) => {
 
+    languages[2] = "Javascript";
+
+  return languages;
 }
 
 const addElement = (languages) => {
 
+    languages[4] = "Ruby";
+    languages[5] = "Python";
+    return languages;
 }
 
 const addNumberElement = (numbers) => {
 
+    numbers.unshift(-1);
+    numbers.unshift(-2);
+    return numbers;
 }
 
 const removeFirst = (languages) => {
-
+    languages.shift();
+    return languages;
 }
 
 const removeLast = (languages) => {
-
+    languages.pop();
+    return languages;
 }
 
 const convertStrToArr = (social_arr) => {
-
+    return social_arr.split(",");
 }
 
 const convertArrToStr = (languages) => {
-
+    return languages.join(",");
 }
 
 const sortArr = (social_arr) => {
-
+    return social_arr.sort();
 }
 
 const invertArr = (social_arr) => {
-
+    return social_arr.reverse();
 }
