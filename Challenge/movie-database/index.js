ddfd0db6eaf3f83b2df177fe429075780e33994c
tinamechleb <hackour2@hotmail.com>
2020-01-23 11:29:39 +0200

    Step-1

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/*", (req, res) => res.send("ok"));
+app.get("/", (req, res) => res.send("ok"));
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
