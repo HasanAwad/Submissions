b19b9ef7f1e8ed7c77a58b0e7670a95fd9fc779c
tinamechleb <hackour2@hotmail.com>
2020-01-24 06:43:58 +0200

    step4

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -12,4 +12,21 @@ app.get("/time", function(req, res) {
   res.send({ status: 200, message: date.getDate() + ":" + date.getSeconds() });
 });
 
+app.get(`/hello/:id`, function(req, res) {
+  const id = req.params.id;
+  res.send({ status: 200, message: `hello , ${id}` });
+});
+
+app.get("/search", (req, res) => {
+  if (req.query.s) {
+    res.send({ status: 200, message: "ok", data: req.query.s });
+  } else {
+    res.send({
+      status: 500,
+      error: true,
+      message: "you have to provide a search"
+    });
+  }
+});
+
 app.listen(port, () => console.log(`app listening on port ${port}!`));
